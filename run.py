37c4ad4d38591f49f6aef7e02505d91993877645
aleksem07 <89778523+aleksem07@users.noreply.github.com>
2025-10-02 11:46:38 +0300

    feat
    add access by ip, hostname

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ from src.ldap_auth.config import Config
 
 app = create_app()
 
-
 if __name__ == '__main__':
     app.run(
         host=Config.HOST,
--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { HOSTNAME } from 'os'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3000,
+    strictPort: false,
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      '::1',
+      HOSTNAME,
+      `${HOSTNAME}.local`,
+    ],
     proxy: {
       '/auth': {
         target: 'http://localhost:5000',
